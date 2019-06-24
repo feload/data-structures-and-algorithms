c9b7a3ce17e72a77700bf0c106b137e4b7f6654c
Felipe <feload@gmail.com>
2019-06-24 08:09:33 -0500

    Add Queue from Stack solution

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,45 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+
+    constructor () {
+        this.stackA = new Stack()
+        this.stackB = new Stack()
+    }
+
+    add (item) {
+        this.stackA.push(item)
+    }
+
+    remove () {
+        while (this.stackA.peek()){
+            this.stackB.push(this.stackA.pop())
+        }
+
+        const returnValue = this.stackB.pop()
+
+        while (this.stackB.peek()){
+            this.stackA.push(this.stackB.pop())
+        }
+
+        return returnValue
+    }
+
+    peek () {
+        while (this.stackA.peek()){
+            this.stackB.push(this.stackA.pop())
+        }
+
+        const returnValue = this.stackB.peek()
+
+        while (this.stackB.peek()){
+            this.stackA.push(this.stackB.pop())
+        }
+
+        return returnValue
+    }
+
+}
 
 module.exports = Queue;
