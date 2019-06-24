67087c61f4a4f41c771b6c4d55c0373ad6695c94
Felipe <feload@gmail.com>
2019-06-23 22:57:16 -0500

    Add Queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor () {
+        this.items = []
+    }
+
+    add (item) {
+        this.items.push(item)
+    }
+
+    remove () {
+        return this.items.shift()
+    }
+}
 
 module.exports = Queue;
